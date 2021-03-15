af16a1e51012bdcfce171d9670193a9c3701cbb2
JanPfenning <jan.pfenning@gmail.com>
2021-03-15 13:41:12 +0100

    prerequists

--- swagger-demo/src/main.ts
+++ swagger-demo/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Copy the following code to your projects main.ts
+  const config = new DocumentBuilder()
+    .setTitle('Swagger-Demo')
+    .setDescription('The description for your API')
+    .setVersion('1.0')
+    .addTag('public', 'Controller for public access')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // Up to here
+
   await app.listen(3000);
 }
 bootstrap();
